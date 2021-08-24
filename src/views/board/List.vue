87b414e3475613556df5b173eb54b061b5a71eb8
onethej <jhjang951@gmail.com>
2021-08-24 23:16:56 +0900

    게시판 목록 샘플코드 추가

--- src/views/board/List.vue
+++ src/views/board/List.vue
@@ -1,14 +1,98 @@
 <template>
   <div class="board-list">
-    게시판 목록이 나타날 공간입니다.
+    <table>
+      <thead>
+      <tr>
+        <th>No</th>
+        <th>제목</th>
+        <th>작성자</th>
+        <th>등록일시</th>
+      </tr>
+      </thead>
+
+      <tbody>
+      <tr v-for="(row, idx) in list" :key="idx">
+        <td>{{ no - idx }}</td>
+        <td>{{ row.title }}</td>
+        <td>{{ row.author }}</td>
+        <td>{{ row.createdAt }}</td>
+      </tr>
+      </tbody>
+    </table>
+    <div>
+      <span>&lt;&lt;</span>
+      &nbsp;
+      <span>&lt;</span>
+      &nbsp;
+      1
+      &nbsp;
+      <span>&gt;</span>
+      &nbsp;
+      <span>&gt;&gt;</span>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'List',
-  components: {
+  data() { //변수생성
+    return {
+      requestBody: {}, //리스트 페이지 데이터전송
+      list: {}, //리스트 데이터
+      no: '', //게시판 숫자처리
+      paging: {}, //페이징 데이터
+      page: this.$route.query.page ? this.$route.query.page : 1,
+      size: this.$route.query.size ? this.$route.query.size : 10,
+      keyword: this.$route.query.keyword,
+    }
+  },
+  mounted() {
+    this.fnGetList()
+  },
+  methods: {
+    fnGetList() {
+      /************************************************
+       axios 로 backend 호출해서 데이터 가져올 예정
+       ************************************************/
+      //axios body에 requestBody 추가해서 요청하기
+      // this.requestBody = { // 데이터 전송
+      //   keyword: this.keyword,
+      //   page: this.page,
+      //   size: this.size
+      // }
+
+      this.no = 3
+      this.list = [
+        {
+          idx: 3,
+          title: '게시글3',
+          author: '작성자3',
+          createdAt: '2021-08-24 23:30:00'
+        },
+        {
+          idx: 2,
+          title: '게시글2',
+          author: '작성자2',
+          createdAt: '2021-08-24 23:20:00'
+        },
+        {
+          idx: 1,
+          title: '게시글1',
+          author: '작성자1',
+          createdAt: '2021-08-24 23:10:00'
+        }
+      ]
+    },
+
   }
 }
 </script>
+
+<style scoped>
+
+.board-list {
+  width: 400px;
+  margin: auto;
+}
+
+</style>
